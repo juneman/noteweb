1263c3a973a6e228eead9d3e21ce18d7df99e416
dubin <dubingo201@163.com>
2014-02-23 21:17:02 +0800

    update editor

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,144 +62,25 @@ function rtrim(str) {
 }
 
 
-
 /**
  * 0 : view 
  * 1 : edit 
  */
-
-var g_editor_mode = 0; 
-function showEditor(title, content) {
-	$("#yy-wysiwyg-editor #editor").html(content);
-	$('#title-in-edit-mode input').val(trim(title));
-	
-	$('#content').css('display', 'none');
-	$("#yy-wysiwyg-editor").css('display', 'inline-block');
-
-	$('#btnEdit').css('display', 'none');
-	$('#btnSave').css('display', 'inline-block');
-
-	$('#yy-edit-article-title #title-in-view-mode').css('display', 'none');
-	$('#yy-edit-article-title #title-in-edit-mode').css('display', 'inline-block');
-	g_editor_mode = 1;
-}
-
-function showViewer(title, content) {
-	$("#yy-edit-article-title #title-in-view-mode p#title0").text(title);
-	$("#content").html(content);
-	
-	$('#content').css('display', 'inline-block');
-	$("#yy-wysiwyg-editor").css('display', 'none');
-
-	$('#btnEdit').css('display', 'inline-block');
-	$('#btnSave').css('display', 'none');
-
-	$('#yy-edit-article-title #title-in-view-mode').css('display', 'inline-block');
-	$('#yy-edit-article-title #title-in-edit-mode').css('display', 'none');
-	
-	g_editor_mode = 0;
-}
-
-function hideAlertInputTitleMsg() {
-	var obj = $("#alert-msg");
-		obj.fadeOut("500", function() {
-		});
-}
-
-function showAlertInputTitleMsg(now) {
-	var obj = $("#alert-msg");
-	if (now) {
-		obj.css('display', 'inline');
-	}
-	else {
-		obj.fadeIn("500", function() {
-		});
-	}
-}
-
-function edit(str) {
-	var contents = $('#content').html();
-	var title = trim($('#title-in-view-mode #title0').text());
-	
-	showEditor(title, contents);
-}
-
-function editNew() {
-		if (g_editor_mode == 1) {
-			var ret = SaveAndCloseAllEditors("");
-			if (ret == 1) {
-				return;
-			}
-		}
-		
-			
-	$("#btnEditNew").hide();
-	$("#btnEdit").show();
-	$("#btnDel").show();
-		
-		var actived = $('#list-detail-items .yy-list-items ul').find("li.active");
-		if (actived.length > 0) 
-		{
-			actived.removeClass("active");
-		}
-		
-		var htmlContent = '<li class="active" data-section="1">'
-						+ '<div class="title"> Unknown Title'
-						+ '</div> '
-						+ '<div class="description">'
-						+ '<span class="date">2013-10-15</span>'
-						+ '<span class="summary">Our life is important....</span>'
-						+ '</div>'
-						+ '</li>';
-		
-		$("#list-detail-items .yy-list-items ul").prepend(htmlContent);
-		
-		var total = $('#list-detail-items .yy-list-items ul').find("li");
-		
-		if (total.length > 5) {
-			$("#list-detail-items .yy-list-items ul li").last().remove();
-		}
-	
-	$('#title-in-edit-mode input').attr("placeholder", "Input new title");
-	showEditor("", "");
-
-}
-
-function SaveAndCloseAllEditors(str) {
+function saveAndCloseAllEditors() {
 	var title = trim($("#title-in-edit-mode input").val());
 	if (title == "") {
-		showAlertInputTitleMsg(true);
 		return 1;
 	}
 	var contents = $("#yy-wysiwyg-editor #editor").html();
- 	showViewer(title, contents);
+ 	if (trim(contents) == "") {
+ 		return 1;
+ 	}
  	
-	var summary = trim($("#yy-wysiwyg-editor #editor").text()).substr(0, 45) + " ...";
-	
-	$("#list-detail-items .yy-list-items ul li.active").find("div.title").text(title);
-	$("#list-detail-items .yy-list-items ul li.active").first().find("div.description span.summary").html(summary);
-	
+	alert(contents);
 	return 0;
 }
 
-
-function adjustEditorToolbar() {
-	var o = document.getElementById("editor");
-	var w = o.offsetWidth;  //宽度
-	
-	$('#yy-note-title-input').css('width', w + 'px');
-	$('#wysiwyg-toolbar').css('width', w + 'px');
+function clearAllEditor() {
+	$("#title-in-edit-mode input").val("");
+	$("#yy-wysiwyg-editor #editor").html("");
 }
-
-$(function() {
-	
-	$('#title-in-edit-mode input').click(function() {
-		hideAlertInputTitleMsg();
-		
-	});
-	$('#editor').click(function() {
-		hideAlertInputTitleMsg();
-	});
-	
-	
-});
