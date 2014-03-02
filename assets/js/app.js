267a30d2b5a5702e251c3243f510fc10975838fb
dubin <dubingo201@163.com>
2014-03-02 17:43:36 +0800

    add comment system webpage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,54 +1,64 @@
 /**
  * @author dubin
  */
-
-/*
-$(function() {
-	var showOrHideNavbar = function() {
-		var st = $(window).scrollTop();
-		var top = parseInt($('.navbar').css('top'));
-
-		if (st <= 90 && top < -10) {
-			$('.navbar').css({
-				'top' : -60
-			}).delay(10).animate({
-				'top' : 0
-			}, 200);
-		} else if (st > 100 && top >= -5) {
-			$('.navbar').css({
-				'top' : 0
-			}).delay(10).animate({
-				'top' : -60
-			}, 200);
-		}
-	};
-
-	var showOrHidePostNewBlog = function() {
-		var st = $(window).scrollTop();
-		if (st <= 90) {
-			$('.post-new-blog-navbar-btn-wrap').css('display', 'none');
-			$('.post-new-blog-btn').css('display', 'block');
-			
-		} else if (st > 100) {
-			$('.post-new-blog-navbar-btn-wrap').css('display', 'block');
-			$('.post-new-blog-btn').css('display', 'none');
-			
-		}
-	};
-
-	$(window).bind("scroll", showOrHidePostNewBlog);
-});
-*/
-
-/**
+/***
+ * 
+ * back to Top
  * 
  */
-/**
- *
- * @auth: db, created at 2013.10.19
- *        www.yynote.com
- *
- */
+
+
+var goto_top_type = -1;
+var goto_top_itv = 0;
+ 
+function goto_top_timer() {
+var y = goto_top_type == 1 ? document.documentElement.scrollTop : document.body.scrollTop;
+var moveby = 15;
+ 
+y -= Math.ceil(y * moveby / 100);
+if (y < 0) {
+y = 0;
+}
+ 
+if (goto_top_type == 1) {
+document.documentElement.scrollTop = y;
+}
+else {
+document.body.scrollTop = y;
+}
+ 
+if (y == 0) {
+clearInterval(goto_top_itv);
+goto_top_itv = 0;
+}
+}
+ 
+function goto_top()
+{
+if (goto_top_itv == 0) {
+if (document.documentElement && document.documentElement.scrollTop) {
+goto_top_type = 1;
+}
+else if (document.body && document.body.scrollTop) {
+goto_top_type = 2;
+}
+else {
+goto_top_type = 0;
+}
+ 
+if (goto_top_type > 0) {
+goto_top_itv = setInterval('goto_top_timer()', 50);
+}
+}
+}
+
+// end of back to Top 
+///////////////////////////////////////////////////////////
+
+$(function() {
+	$('.cmt-vote').tooltip();	
+});
+ 
 function trim(str) { 
 　　     return str.replace(/(^\s*)|(\s*$)/g, "");
 }
@@ -60,8 +70,7 @@ function ltrim(str) {
 function rtrim(str) { 
 　　     return str.replace(/(\s*$)/g,"");
 }
-
-
+ 
 /**
  * 0 : view 
  * 1 : edit 
@@ -104,5 +113,4 @@ function onCommentTextAreaFoucsOut() {
 
 function onCommentActionSubmitBtnClick() {
 	var obj = event.srcElement;
-
 }
