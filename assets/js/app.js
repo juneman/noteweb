dcf4fab25eef8d0723f70f11d79536d5604f81e6
dubin <dubingo201@163.com>
2014-03-22 13:06:20 +0800

    new style

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,91 +6,24 @@
  * back to Top
  * 
  */
-var goto_top_type = -1;
-var goto_top_itv = 0;
-var g_scrolling = 0;
-
-function goto_top_timer() {
-var y = goto_top_type == 1 ? document.documentElement.scrollTop : document.body.scrollTop;
-var moveby = 25; // 15
- 
-y -= Math.ceil(y * moveby / 100);
-if (y < 10) { //if (y < 10)
-y = 0;
-}
- 
-if (goto_top_type == 1) {
-document.documentElement.scrollTop = y;
-}
-else {
-document.body.scrollTop = y;
-}
- 
-if (y == 0) {
-clearInterval(goto_top_itv);
-goto_top_itv = 0;
-g_scrolling = 0;
-}
-}
- 
-function goto_top()
-{
-if (goto_top_itv == 0) {
-if (document.documentElement && document.documentElement.scrollTop) {
-goto_top_type = 1;
-}
-else if (document.body && document.body.scrollTop) {
-goto_top_type = 2;
-}
-else {
-goto_top_type = 0;
-}
- 
-if (goto_top_type > 0) {
-goto_top_itv = setInterval('goto_top_timer()', 50);
-}
-}
-}
-
 $(function() {
-	$('.btn-back-toTop').click(function(){
-		$('.btn-back-toTop').css('display', 'none');
-		g_scrolling = 1;
-		goto_top();
+	// GO TO TOP SETTING
+	var offset = 220;
+	var duration = 500;
+	jQuery(window).scroll(function() {
+		if (jQuery(this).scrollTop() > offset) {
+			jQuery('.btn-back-toTop').fadeIn(duration);
+		} else {
+			jQuery('.btn-back-toTop').fadeOut(duration);
+		}
 	});
-	
-	var showOrHideScrollbar = function() {
-		if (g_scrolling == 1) return true;
 		
-		var st = $(window).scrollTop();
-		var showOrHide = $('.btn-back-toTop').css('display');
-		
-		if  (st > 301 && showOrHide == 'none') {
-	 		$('.btn-back-toTop').css({
-				'opacity' : 0, 
-				'display' : 'block'
-			}).animate({
-				'opacity' : 0.8,
-			}, 300);
-		}
-		
-		if (st < 300  && showOrHide == 'block') {
-			$('.btn-back-toTop').css({
-				'opacity' : 0.8,
-			}).animate({
-				'opacity' : 0,
-			}, 300, function() {
-			  	$('.btn-back-toTop').css('display', 'none');
-			});
-		}
-	};
-
-	$(window).bind("scroll", showOrHideScrollbar);
+	jQuery('.btn-back-toTop').click(function(event) {
+		event.preventDefault();
+		jQuery('html, body').animate({scrollTop: 0}, duration);
+		return false;
+	})
 });
-
-
-
-
 // end of back to Top 
 ///////////////////////////////////////////////////////////
 
